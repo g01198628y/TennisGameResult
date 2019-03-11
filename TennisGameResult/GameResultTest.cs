f8d25dd00d563ba0762905b15a135c249b01ee53
gary.lai99 <gary.lai99@TI03NB133>
2019-03-11 12:26:11 +0800

    Add GetGameResult_0vs0_Should_Return_LoveAll TestMethod

---
+++ TennisGameResult/GameResultTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TennisGameResult
+{
+    [TestClass]
+    public class GameResultTest
+    {
+        [TestMethod]
+        public void GetGameResult_0vs0_Should_Return_LoveAll()
+        {
+            var scores = new ScoreTable();
+            scores.p1 = 0;
+            scores.p2 = 0;
+            var result = scores.score();
+            Assert.AreEqual("AllLove",result);
+        }
+    }
+
+    public class ScoreTable
+    {
+        public int p1 { get; set; }
+        public int p2 { get; set; }
+
+        public string score()
+        {
+            return "AllLove";
+        }
+    }
+}
